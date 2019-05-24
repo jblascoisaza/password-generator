2a938251ca43a611a61493f89c741069f97999d5
Javier Blasco Isaza <jblascoisa@icloud.com>
2019-05-24 21:45:15 +0200

    random characters functions

--- api/functions/test.js
+++ api/functions/test.js
@@ -1,5 +1,42 @@
 module.exports = {
   helloWorld: function(res) {
     res.send("Hello World.");
+  },
+  rNumber: function(max, min) {
+    const number = Math.round(Math.random() * (max - min) + min);
+
+    return number;
+  },
+  rNumbers: function(nLength) {
+    const numbers = [];
+
+    for (let index = 1; index <= nLength; index++) {
+      const number = this.rNumber(9, 1);
+
+      numbers.push(number);
+    }
+
+    return numbers;
+  },
+  rLetter: function() {
+    const lower = "abcdefghijklmnñopqrstuvwxyz";
+    const capital = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    const type = this.rNumber(2, 1);
+    const number = this.rNumber(26, 0);
+    var letter;
+
+    if (type === 1) letter = lower.charAt(number);
+    else letter = capital.charAt(number);
+
+    return letter;
+  },
+  rSpecial: function() {
+    const characters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+
+    const number = this.rNumber(28, 0);
+    const character = characters.charAt(number);
+
+    return character;
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const test = require("./functions/test");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   test.helloWorld(res);
+});
+
+app.get("/api/v1/rNumber", (req, res) => {
+  const number = test.rNumber(9, 1);
+
+  return res.send(number.toString());
+});
+
+app.post("/api/v1/rNumbers", (req, res) => {
+  const numbers = test.rNumbers(req.body.nLength);
+
+  return res.send(numbers);
+});
+
+app.get("/api/v1/rLetter", (req, res) => {
+  const letter = test.rLetter();
+
+  return res.send(letter);
+});
+
+app.get("/api/v1/rSpecial", (req, res) => {
+  const character = test.rSpecial();
+
+  return res.send(character);
 });
 
 app.listen(port, () =>
